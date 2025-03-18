1b1b5bee522e726218a1ae0746f78b98d88522ae
unknown <rodriguezvacaalanrider@gmail.com>
2025-03-18 12:23:19 -0400

    cambios

--- appDisweb/src/app/app-routing.module.ts
+++ appDisweb/src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './pages/inicio/inicio.component';
+import { acercadeComponent } from './pages/acercade/acercade.component';
 
-const routes: Routes = [];
+const routes: Routes = [{path:'',component:InicioComponent},
+  {path:'acercade',component:acercadeComponent}];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes,{useHash:true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
